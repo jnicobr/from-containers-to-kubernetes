4262e01a201e252ba438b172bc58460acf8f437b
Nicolas Bustos <jnbustosr@unal.edu.co>
2022-02-21 17:42:43 -0500

    app.js & static files created

---
+++ app.js
@@ -0,0 +1,32 @@
+// Express App & Router object
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+// Base directory & ports
+const path = __dirname + '/views/';
+const port = 8080;
+
+// log routes requests and passes on to the app's routes
+router.use(function (req, res, next) {
+    console.log('/' + req.method);
+    next();
+})
+
+// App's routes
+router.get('/', function (req, res) {
+    res.sendFile(path + 'index.html');
+});
+
+router.get('/sharks', function (req, res) {
+    res.sendFile(path + '/sharks.html');
+});
+
+// Mount app's assets & router
+app.use(express.static(path));
+app.use('/', router);
+
+// Listen to port 8080
+app.listen(port, function () {
+   console.log('Example app listening  on port 8080!'); 
+});
