d2c25a7bc58559719dcb2bc59efc2ed1ec9c236c
Nicolas Bustos <jnbustosr@unal.edu.co>
2022-02-28 12:43:40 -0500

    Adding options & error handling to the db conn

--- db.js
+++ db.js
@@ -10,9 +10,23 @@ const {
     MONGO_DB 
 } = process.env;
 
+// Connection options
+const options = {
+    useNewUrlParser: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 500,
+    connectTimeoutMS: 10000,
+};
+
 // Declare URI
 // authSource=admin is required to could use user & pass. Is the name of the db with the credentials
 const URI = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
-// Create the connection
-mongoose.connect(URI, {userNewUrlParser: true});
+// Create the connection handling connection failure
+mongoose.connect(URI, options)
+    .then(function() {
+        console.log('MongoDB is connected');
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
