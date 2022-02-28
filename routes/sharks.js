82ac7a498555a5b460f4eba92f6f31463898cf70
Nicolas Bustos <jnbustosr@unal.edu.co>
2022-02-28 13:59:23 -0500

    Fixed missing routes

--- routes/sharks.js
+++ routes/sharks.js
@@ -6,11 +6,11 @@ router.get('/', function (req, res) {
    shark.index(req, res);
 });
 
-router.post('/', function (req, res) {
+router.post('/addshark', function (req, res) {
     shark.create(req, res);
 });
 
-router.get('/', function (req, res) {
+router.get('/getshark', function (req, res) {
     shark.list(req, res);
 })
 
